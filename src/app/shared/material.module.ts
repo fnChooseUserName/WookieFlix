d3abcd1ff141a051828fbe1096da84fe11c48c58
Kieron <kparkinson@riskcontroltech.com>
2022-06-20 16:21:17 -0400

    Added and imported Material library. Created Shared module declaration for Material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+
+import { MatButton, MatButtonModule } from '@angular/material/button'
+
+@NgModule({
+  imports: [
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
+  ]
+})
+export class MaterialModule { }
